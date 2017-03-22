29085c64aef023700df7a83ca9c00d851f1231ea
Dilip02 <c0687174@mylambton.ca>
2017-03-22 11:33:25 -0400

    created model class for all entity

---
+++ src/test/java/GroupMem.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author c0687174
+ */
+public class GroupMem {
+    private int group_id;
+    private int user_id;
+
+    public int getGroup_id() {
+        return group_id;
+    }
+
+    public void setGroup_id(int group_id) {
+        this.group_id = group_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+    
+    
+}
