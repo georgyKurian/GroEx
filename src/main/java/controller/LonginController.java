f4a1ad08b8ee4ddf5a0008e3f1bcd04600ccfb2a
Dilip02 <TOSHIBA@TOSHIBA-TOSH>
2017-04-01 17:32:50 -0400

    Added controller package,Added loginController

--- src/main/java/controller/LonginController.java
+++ src/main/java/controller/LonginController.java
@@ -1,3 +1,6 @@
+
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,8 @@
  * @author TOSHIBA
  */
 public class LonginController {
+    @SessionScoped
+    
+    private List<User> users;
     
 }
